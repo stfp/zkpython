f599374b7b26e8007f5e471bcfd2cb749f94535d
Stefan Praszalowicz <stefan@greplin.com>
2011-05-27 15:18:55 -0700

    use setuptools

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 
 zookeepermodule = Extension("zookeeper",
